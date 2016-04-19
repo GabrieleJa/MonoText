9f3bc8e6bde6a1f6242e12dc9ebcf1746488f2ae
Justas Trimailovas <j.trimailovas@gmail.com>
2016-04-19 11:15:26 +0300

    Add mutation observer to catch changes in the DOM
    E.g. when there are dynamic changes in the website, we would like to
    find textareas and change their font style be monospaced.

--- findTextarea.js
+++ findTextarea.js
@@ -1,5 +1,30 @@
-var textareas = document.getElementsByTagName("textarea");
+// finds and sets textareas to use monospaced font
+var monospaceTextareas = function() {
+  var textareas = document.getElementsByTagName("textarea");
 
-for (i = 0; i < textareas.length; i++) {
-    textareas[i].style.fontFamily = "monospace";
+  for (i = 0; i < textareas.length; i++) {
+      textareas[i].style.fontFamily = "monospace";
+  }
 }
+
+// sets MutationObserver to look for dynamic textareas in the body of html
+var setMutationObserver = function() {
+  // select the target node for mutation observer
+  var target = document.body;
+
+  // create an observer instance
+  var observer = new MutationObserver(function(mutations) {
+    monospaceTextareas();
+  });
+
+  // configuration of the observer:
+  var config = { attributes: true, childList: true, subtree: true, characterData: true };
+
+  // pass in the target node, as well as the observer options
+  observer.observe(target, config);
+}
+
+// set textareas to be monospaced on page load
+// and then set the mutation observer
+monospaceTextareas();
+setMutationObserver();
